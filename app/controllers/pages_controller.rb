c0dd3032de0db8c8d4275806e92925b32d2bc269
Auston <auston.jin@gmail.com>
2014-03-02 22:16:21 -0500

    Initial commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,11 @@
+class PagesController < ApplicationController
+  def home
+  	@message = "Bobby"
+  end
+
+  def about
+  end
+
+  def test
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rasite::Application.config.secret_key_base = '9fc68f34678643721a8c41245442f9f36f3ea1f566b9a4d6345abe1d3321610f700ceb7e79b08aa29db3197b6da3ab4fb1f98e478b45034200708d00662af401'
